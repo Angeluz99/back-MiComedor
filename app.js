c7d09a2d259f9da70cecd5f4d8f2ac4ebea757bd
regeneratecelulas <regeneratecelulasmadre@gmail.com>
2024-07-28 19:48:53 -0600

    vercel.json

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.use(cors());
 app.use(express.json());
 
 // console.log('MongoDB URI:', process.env.MONGODB_URI); 
+app.get('/', (req, res) => {
+  res.send('Hello, MiComedor!');
+});
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected successfully.'))
